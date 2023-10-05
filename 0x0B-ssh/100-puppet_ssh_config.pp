9d3321980b19de6003cd2735de5093daf14b745b
kokainaW <williamkhoko@yahoo.com>
2023-10-06 00:47:51 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-#sets up your client SSH configuration fie
-file_line { 'Turn_off_passwd_auth':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  }
+# sets up your client SSH configuration file
 
-file_line { 'Declare_identity_file':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  }
+file { "etc/ssh/ssh_config":
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
